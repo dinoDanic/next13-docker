723ce357630a8e2fcd2c7b3457fa96d9f5cbb265
dinoDanic <dino.danic@gmail.com>
2023-05-28 22:07:55 +0200

    test

---
+++ components/triger-me.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const TrigerMe = () => {
+  return (
+    <div>triger-me</div>
+  )
+}
--- features/sidebar/components/sidebar.tsx
+++ features/sidebar/components/sidebar.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { SomeComponent } from "./some-component/some-component";
+import { TrigerMe } from "@/components/triger-me";
 
 export const Sidebar = () => {
   return (
     <div>
       <SomeComponent />
+      <TrigerMe />
     </div>
   );
 };
